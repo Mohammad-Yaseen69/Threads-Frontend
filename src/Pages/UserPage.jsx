5c3c97a5f827b93a445ccb30219f8c341b057d5c
Mohammad-Yaseen69 <yaseenyaseenali69@gmail.com>
2024-09-05 16:18:16 +0500

    Add - Building User Header in profile page with follow unfollow

--- src/Pages/UserPage.jsx
+++ src/Pages/UserPage.jsx
@@ -1,19 +1,61 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
-import { UserHeader , UserPost} from '../components'
+import { UserHeader, UserPost } from '../components'
+import { makeRequest } from '../Utils/api'
+import { useToast } from '@chakra-ui/react'
+import { useRecoilState } from 'recoil'
+import { userAtom } from '../Atoms/user'
 
 const UserPage = () => {
-    const { userId } = useParams()
+    const { userName } = useParams()
+    const [user, setUser] = useState()
+    const [loggedinUser] = useRecoilState(userAtom)
+    const toast = useToast()
+
+    useEffect(() => {
+        async function getUserProfile() {
+            const response = await makeRequest(`users/profile/${userName}`)
+
+            if(response.error){
+                toast({
+                    title: "Error",
+                    description: response.error.message,
+                    status: "error",
+                    duration: 3000,
+                    isClosable: true,
+                })
+            }else{
+                setUser(response.response.data[0])
+            }
+
+            console.log(response)
+        }
+
+        getUserProfile()
+    }, [userName])
+
+
+
+    console.log(user)
+
 
     return (
         <div>
-           <UserHeader 
-           />
+            <UserHeader
+                userName = {user?.userName}
+                fullName = {user?.name}
+                avatar = {user?.pfp?.url}
+                bio= {user?.bio}
+                instagram = {user?.instagram}
+                followers = {user?.followersCount}    
+                followed={user?.followed} 
+                userId={user?._id}          
+            />
 
-           <UserPost userId={userId} likes='1221' replies='122' postImg='/post1.png' postTitle="this is first post"/>
-           <UserPost userId={userId} likes='121' replies='12' postImg='/post2.png' postTitle="this is secpnd post"/>
-           <UserPost userId={userId} likes='121' replies='12' postImg='/post3.png' postTitle="this is secpnd post"/>
-           <UserPost userId={userId} likes='12221' replies='1'  postTitle="without img post"/>
+            <UserPost userId={user?._id} likes='1221' replies='122' postImg='/post1.png' postTitle="this is first post" />
+            <UserPost userId={user?._id} likes='121' replies='12' postImg='/post2.png' postTitle="this is secpnd post" />
+            <UserPost userId={user?._id} likes='121' replies='12' postImg='/post3.png' postTitle="this is secpnd post" />
+            <UserPost userId={user?._id} likes='12221' replies='1' postTitle="without img post" />
 
 
         </div>
--- src/components/UserHeader.jsx
+++ src/components/UserHeader.jsx
@@ -1,13 +1,90 @@
-import { Avatar, Box, Flex, Link, Menu, MenuButton, MenuItem, MenuList, Portal, Text, useColorMode, useToast, VStack } from '@chakra-ui/react'
+import { Avatar, Box, Button, Flex, Link, Menu, MenuButton, MenuItem, MenuList, Portal, Text, useColorMode, useToast, VStack } from '@chakra-ui/react'
 import { BsInstagram } from "react-icons/bs"
 import { CgMoreO } from "react-icons/cg"
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { color } from 'framer-motion'
+import { makeRequest } from '../Utils/api'
 
-const UserHeader = () => {
+const UserHeader = ({
+    userName,
+    bio,
+    fullName,
+    followers,
+    instagram,
+    avatar,
+    followed,
+    userId
+}) => {
     const toast = useToast()
     const [activeTab, setActiveTab] = useState('threads')
-    const {colorMode} = useColorMode()
+    const { colorMode } = useColorMode()
+    const [followedState, setFollowedState] = useState(followed)
+    const [followerState, setFollowerState] = useState(followers)
+    const [followFormat, setFollowFormat] = useState("")
+
+    console.log(followedState)
+
+    useEffect(() => {
+        setFollowedState(followed)
+    }, [followed])
+
+    useEffect(() => {
+        setFollowerState(followers)
+    }, [followers])
+
+
+
+    console.log(userId)
+    useEffect(() => {
+        const changeFollowersFormat = () => {
+            if (followerState > 1000000) {
+                console.log("million")
+                setFollowFormat((followerState / 1000000).toFixed(1) + 'm')
+            }
+            // write this function for changing followers formats to numbers to "k" "m"
+            if (followerState > 1000) {
+                setFollowFormat((followerState / 1000).toFixed(1) + 'k')
+            }
+
+            return setFollowFormat(followerState)
+        }
+
+        changeFollowersFormat()
+    }, [followers, followedState])
+
+    const handleFollow = async () => {
+        if (followedState) {
+            setFollowedState(false)
+            setFollowerState(followerState - 1)
+        } else {
+            setFollowedState(true)
+            setFollowerState(followerState + 1)
+        }
+
+        const response = await makeRequest(`users/follow/${userId}`, {
+            method: "POST"
+        })
+
+        if (response.error) {
+            toast({
+                title: 'Error',
+                description: response.error.message,
+                status: 'error',
+                duration: 3000,
+                isClosable: true,
+            })
+        }
+        else {
+            toast({
+                title: 'Success',
+                description: !followedState ? "User Followed" : "User Unfollowed",
+                status: 'success',
+                duration: 3000,
+                isClosable: true,
+            })
+        }
+    }
+
 
     const copyUrl = () => {
         const currentUrl = window.location.href;
@@ -25,49 +102,52 @@ const UserHeader = () => {
             <Flex justifyContent={'space-between'} alignItems={'center'} w={'full'}>
                 <Box>
                     <Text fontSize={'2xl'} fontWeight={700}>
-                        Mark Zuckerberg
+                        {fullName}
                     </Text>
 
                     <Flex alignItems={'center'} gap={2}>
-                        <Text fontSize={'sm'}>markzuckerberg</Text>
-                        <Text fontSize={'xs'} bg={'gray.dark'} color={'gray.light'} p={1} borderRadius={'full'}>
-                            Threads.net
-                        </Text>
+                        <Text fontSize={'sm'}>{userName}</Text>
                     </Flex>
                 </Box>
 
                 <Box>
                     <Avatar
-                        name='mark zuckerberg'
-                        src='../public/zuck-avatar.png'
+                        name={fullName}
+                        src={avatar}
                         size={"xl"}
                     />
                 </Box>
             </Flex>
 
-            <Text >Ceo of Facebook </Text>
+            <Text >{bio} </Text>
 
             <Flex flexDirection={{
                 base: 'column',
                 sm: 'row'
-            
+
             }} gap={3} justifyContent={'space-between'} w={'full'}>
                 <Flex alignItems={'center'} gap={2}>
-                    <Text color={'gray.light'}>3.7k followers</Text>
-                    <Box w={1} h={1} borderRadius={'full'} bg={'gray.light'}></Box>
-                    <Link color={'gray.light'}>instagram.com</Link>
+                    <Text color={'gray.light'}>{followFormat} followers</Text>
+                    <Button _hover={!followedState && "blue.600"} onClick={handleFollow} backgroundColor={!followedState && "blue.700"}>
+                        {followedState ? "Unfollow" : "Follow"}
+                    </Button>
                 </Flex>
 
                 <Flex alignItems={'center'}>
-                    <Box w={'40px'} h={'40px'} className={colorMode === "dark" ?  'header-icons' : 'header-icons-light'}>
-                        <BsInstagram size={24} color={colorMode == "light" ? "black" : 'white'} cursor={"pointer"} />
-                    </Box>
-                    <Box w={'40px'} h={'40px'} className={colorMode === "dark" ?  'header-icons' : 'header-icons-light'}>
+
+                    {instagram &&
+                        <a href={instagram} target='_blank'>
+                            <Box w={'40px'} h={'40px'} className={colorMode === "dark" ? 'header-icons' : 'header-icons-light'}>
+                                <BsInstagram size={24} color={colorMode == "light" ? "black" : 'white'} cursor={"pointer"} />
+                            </Box>
+                        </a>
+                    }
+
+                    <Box w={'40px'} h={'40px'} className={colorMode === "dark" ? 'header-icons' : 'header-icons-light'}>
                         <Menu>
                             <MenuButton>
                                 <CgMoreO className='icon' size={24} color={colorMode == "light" ? "black" : 'white'} cursor={"pointer"} />
                             </MenuButton>
-
                             <Portal>
                                 <MenuList bg={colorMode == 'dark' ? 'gray.dark' : 'gray.light'}>
                                     <MenuItem onClick={copyUrl} bg={colorMode == 'dark' ? 'gray.dark' : 'gray.light'}>Copy Link</MenuItem>
@@ -87,7 +167,7 @@ const UserHeader = () => {
                     onClick={() => setActiveTab('threads')}
                     transition={'all 0.3s'}
                 >
-                    <Text fontWeight={'bold'} color={activeTab == 'threads' ? colorMode == 'dark' ? 'white' : 'gray.dark': 'gray.light'}>Threads</Text>
+                    <Text fontWeight={'bold'} color={activeTab == 'threads' ? colorMode == 'dark' ? 'white' : 'gray.dark' : 'gray.light'}>Threads</Text>
                 </Flex>
                 <Flex
                     flex={1}
